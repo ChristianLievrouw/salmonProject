f6d1c6cc5ddf3224f64639c18d125030df17090d
ChristianLievrouw <celievrouw1@gmail.com>
2020-06-22 18:06:31 -0500

    got first store going

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
 'use strict';
 console.log('This file is connected.');
 
+var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Total'];
+
+
+var seattleStore = {
+  minHourCus: 23,
+  maxHourCus: 65,
+  avgCookiePerCus: 6.3,
+  cusPerHour: 0,
+  setCusPerHour: function() {
+    this.cusPerHour = randomNumber(this.minHourCus, this.maxHourCus);
+    console.log(this.cusPerHour, ' customers at');
+  },
+  cookiesSoldPerHour: [],
+  setCookiesPerHour: function() {
+    this.setCusPerHour();
+    this.cookiesSoldPerHour.push(Math.floor((this.cusPerHour) * this.avgCookiePerCus));
+    console.log(this.cookiesSoldPerHour);
+  },
+
+};
+seattleStore.setCusPerHour();
+for(var i = 0; i < storeHours.length - 1; i++){
+  seattleStore.setCookiesPerHour();
+}
+function randomNumber(minHourCus, maxHourCus) {
+  return Math.floor(Math.random() * (maxHourCus - minHourCus + 1) + minHourCus);
+}
+
+
+var mainElement = document.getElementById('list');
+var ul = document.createElement('ul');
+for(var i = 0; i < storeHours.length; i++) {
+  var li = document.createElement('li');
+  li.textContent = 'At ' + storeHours[i] + ' we sold ' + seattleStore.cookiesSoldPerHour[i] + '!';
+  ul.append(li);
+}
+mainElement.appendChild(ul);
+
